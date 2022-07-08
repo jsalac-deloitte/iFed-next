8c5b171b6c3f69d94680fafaae8fe5ecb9a1b0e8
arnoldcostimianoV <arnoldverzo0@gmail.com>
2022-07-08 13:48:01 +0800

    added ADD on industry

--- components/admin/metadata/sub-components/Test2Industry.tsx
+++ components/admin/metadata/sub-components/Test2Industry.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { MdAddCircleOutline, MdEdit, MdDeleteForever } from "react-icons/md";
 import { data } from "../../../Constant";
+import { Iindustry } from "../../../../pages/admin/metadata/Interfaces";
+import Industrytest2 from "../../../../components/admin/metadata/sub-components/Industrytest2";
 const Test2Industry = () => {
+  const [industry, setIndustry] = useState<string>("");
+  const [dataList, setDataList] = useState<Iindustry[]>([]);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setIndustry(event.target.value);
+  };
+
+  const completeIndustry = (industryNameToDelete: string): void => {
+    setDataList(
+      dataList.filter((industry) => {
+        return industry.industryName != industryNameToDelete;
+      })
+    );
+  };
+
+  const addIndustry = (): void => {
+    const newIndustry = { industryName: industry };
+    setDataList([...dataList, newIndustry]);
+    setIndustry("");
+    // console.log(dataList);
+  };
+
   return (
     <div className="">
       <div className="md:flex md:flex-row md:items-center md:justify-center lg:ml-[80px]">
@@ -18,77 +41,32 @@ const Test2Industry = () => {
           </p>
           <div className="flex justify-between px-[50px] py-[20px] md:pt-2">
             <input
+              onChange={handleChange}
               type="text"
-              placeholder="Enter level name..."
+              placeholder="Enter industry name..."
               className="w-[175px] py-[8px] pl-[15px] text-[12px] rounded-[25px] "
             />
-            <button className="bg-green-700 flex items-center text-white text-[12px] rounded-[25px] cursor-pointer">
+            <button
+              onClick={addIndustry}
+              className="bg-green-700 flex items-center text-white text-[12px] rounded-[25px] cursor-pointer"
+            >
               <span className="ml-[15px] mr-[3px]">Add</span>
               <MdAddCircleOutline className="w-[20px] h-[20px] my-[5px] mr-[15px]" />
             </button>
           </div>
         </div>
       </div>
-      <div className="mt-[20px] mx-auto px-[15px] py-[20px] w-[280px] bg-white  rounded-[10px] md:w-[560px] md:h-[344px] md:mt-[50px]">
-        <p className="pl-[10px] pb-[25px] bold text-[16px]">List</p>
-        <div className="py-[10px] border-solid border-b border-gray-500 flex justify-between">
-          <span className="pl-[10px] text-[12px] text-[#53565A]">
-            Analyst joseph
-          </span>
-          <div className="mr-[10px] w-[75px] flex justify-between">
-            <button className="bg-[#0D6EFD] flex items-center text-white text-[6px] md:rounded-[25px] rounded-l-[25px]  cursor-pointer md:-[64px] md:h-[20px] md:text-[12px] md:-ml-8">
-              <span className="ml-[7px] mr-[3px] ">Edit</span>
-              <MdEdit className="w-[10px] h-[10px] my-[3px] mr-[5px]" />
-            </button>
-            <button className="bg-[#DC3545] flex items-center text-white text-[6px] md:rounded-[25px] rounded-r-[25px]  cursor-pointer md:h-[20px] md:text-[12px]">
-              <span className="ml-[5px]">Delete</span>
-              <MdDeleteForever className="w-[10px] h-[10px] my-[3px] mr-[3px]" />
-            </button>
-          </div>
-        </div>
-        <div className="py-[10px] border-solid border-b border-gray-500 flex justify-between">
-          <span className="pl-[10px] text-[12px] text-[#53565A]">
-            Consultant
-          </span>
-          <div className="mr-[10px] w-[75px] flex justify-between">
-            <button className="bg-[#0D6EFD] flex items-center text-white text-[6px] md:rounded-[25px] rounded-l-[25px]  cursor-pointer md:h-[20px] md:text-[12px] md:-ml-8">
-              <span className="ml-[7px] mr-[3px]">Edit</span>
-              <MdEdit className="w-[10px] h-[10px] my-[3px] mr-[5px]" />
-            </button>
-            <button className="bg-[#DC3545] flex items-center text-white text-[6px] md:rounded-[25px] rounded-r-[25px] cursor-pointer md:h-[20px] md:text-[12px]">
-              <span className="ml-[5px]">Delete</span>
-              <MdDeleteForever className="w-[10px] h-[10px] my-[3px] mr-[3px]" />
-            </button>
-          </div>
-        </div>
-        <div className="py-[10px] border-solid border-b border-gray-500 flex justify-between">
-          <span className="pl-[10px] text-[12px] text-[#53565A]">
-            Senior Consultant
-          </span>
-          <div className="mr-[10px] w-[75px] flex justify-between">
-            <button className="bg-[#0D6EFD] flex items-center text-white text-[6px] md:rounded-[25px] rounded-l-[25px] cursor-pointer md:h-[20px] md:text-[12px] md:-ml-8">
-              <span className="ml-[7px]  mr-[3px]">Edit</span>
-              <MdEdit className="w-[10px] h-[10px] my-[3px] mr-[5px]" />
-            </button>
-            <button className="bg-[#DC3545] flex items-center text-white text-[6px] md:rounded-[25px]  rounded-r-[25px] cursor-pointer md:h-[20px] md:text-[12px] ">
-              <span className="ml-[5px]">Delete</span>
-              <MdDeleteForever className="w-[10px] h-[10px] my-[3px] mr-[3px]" />
-            </button>
-          </div>
-        </div>
-        <div className="py-[10px] border-solid border-b border-lineGray flex justify-between">
-          <span className="pl-[10px] text-[12px] text-[#53565A]">Managers</span>
-          <div className="mr-[10px] w-[75px] flex justify-between">
-            <button className="bg-[#0D6EFD] flex items-center text-white text-[6px] md:rounded-[25px] rounded-l-[25px] cursor-pointer md:h-[20px] md:text-[12px] md:-ml-8">
-              <span className="ml-[7px] mr-[3px]">Edit</span>
-              <MdEdit className="w-[10px] h-[10px] my-[3px] mr-[5px]" />
-            </button>
-            <button className="bg-[#DC3545] flex items-center text-white text-[6px] md:rounded-[25px]  rounded-r-[25px]  cursor-pointer md:h-[20px] md:text-[12px]">
-              <span className="ml-[5px]">Delete</span>
-              <MdDeleteForever className="w-[10px] h-[10px] my-[3px] mr-[3px]" />
-            </button>
-          </div>
-        </div>
+
+      <div className="">
+        {dataList.map((industry: Iindustry, key: number) => {
+          return (
+            <Industrytest2
+              key={key}
+              industry={industry}
+              completeIndustry={completeIndustry}
+            />
+          );
+        })}
       </div>
     </div>
   );

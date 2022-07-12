a2908a4883131e4d18c578d9ffe0ba302cb50517
jodevera <jodevera@deloitte.com>
2022-07-12 10:21:00 +0800

    refactored directory 2.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import UserProfile from './Pages/Profile/ReadUser'
-import CreateUser from './Pages/Profile/CreateUser'
-import ReadUser from './Pages/Profile/ReadUser'
+import UserProfile from './Profile/ReadUser'
+import CreateUser from './Profile/CreateUser'
+import ReadUser from './Profile/ReadUser'
 
 const Home: NextPage = () => {
   return (
